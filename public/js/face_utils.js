5f825d4787565c6128476261b29a1cc4c6548580
cys7885 <thcksdud7885@gmail.com>
2020-09-23 08:46:03 -0700

    테스트 코드 추가

--- public/js/face_utils.js
+++ public/js/face_utils.js
@@ -1,5 +1,6 @@
 function opencamera(){
     document.getElementById("face-auth-camera").src = "https://algorithmxlab.com/wp-content/uploads/2019/11/Facial-Recognition-All-you-Need-to-Know.jpg";
+    //document.getElementById("face-auth-camera").src = "http://192.168.1.7:5000/video_feed";
     document.getElementById("face-auth-text-span").innerHTML = '얼굴 인식 중...';
     document.getElementById("reservation-result-area").style="display:none";
 }
@@ -14,10 +15,11 @@ function closecamera(){
     document.getElementById("reservation-status-text").innerHTML = "대기중";
 }
 
-function face_auth_success(){
+function face_auth_success(information){
     document.getElementById("reservation-status-text").className = "reservation-success";
     document.getElementById("reservation-status-mark").className = "fas fa-check-circle fa-3x";
     document.getElementById("reservation-status-mark").style="color:#3BB54A";
+    document.getElementById("jarvisuser").innerHTML = information.username;
     document.getElementById("reservation-result-area").style="display:block";
     document.getElementById("face-auth-text-span").innerHTML = '얼굴인식 완료';
     document.getElementById("reservation-status-text").innerHTML = '인증성공';
