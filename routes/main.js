b9c0c559bbd22c957bd3812f20a30272490b5602
cys7885 <thcksdud7885@gmail.com>
2020-11-19 06:28:26 -0800

    리모트 제어 로고 변경

--- routes/main.js
+++ routes/main.js
@@ -17,24 +17,26 @@ module.exports = function (app, utils) {
 
    app.get('/', function (req, res) {
       var name = req.query.name;
-      var power = req.query.power;
-      var auth_flag = false;
+      //var power = req.query.power;
+      //var auth_flag = false;
 
-      var welcome_voice = utils.welcome_voice;
+      /*var welcome_voice = utils.welcome_voice;
 
       if (name)
          var auth_flag = utils.set_auth(name);
 
       if (power == "1")
          utils.set_engine(power);
-
+      */
+      res.render('index', {auth: name});
+      /*
       if (auth_flag) {
          res.render('index', { auth: name, welcome_voice: welcome_voice });
       }
       else {
          utils.set_auth("");
          res.render('index', { auth: "" });
-      }
+      }*/
    });
    app.get('/dashboard', function (req, res) {
       res.render('dashboard', {session: jarvis_session});
